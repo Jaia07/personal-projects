2d2cbb23e2e2a3e6bb0286b8b266d9a49f16adb0
Joseph Adebanjo <josephadebanjo4@gmail.com>
2025-06-16 06:42:09 +0100

    Implemented functionality to update job listings when a new job is added

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -5,8 +5,20 @@ import { Routes, Route } from "react-router-dom";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import JobPage from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
 
 const App = () => {
+  const addJob = async (newJob) => {
+    const res = await fetch("/api/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
   return (
     <>
       <Navbar />
@@ -14,6 +26,7 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="/jobs/:id" element={<JobPage />} />
+        <Route path="/add-job" element={<AddJobPage addNewJob={addJob} />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
--- react-jobs/src/components/Navbar.jsx
+++ react-jobs/src/components/Navbar.jsx
@@ -1,31 +1,35 @@
-import React from 'react'
-import logo from '../assets/images/logo.png'
+import React from "react";
+import logo from "../assets/images/logo.png";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
-        <nav className='bg-indigo-700 border-b-2 border-indigo-500'>
-          <div className="flex items-center justify-center md:justify-between max-w-5xl w-full mx-auto px-2 sm:px-6 lg:px-8">
-            <section className='flex items-center gap-1'>
-              <img 
-                src={logo} 
-                alt="React Logo"
-                className='h-10 w-auto' 
-              />
-              <span className='hidden sm:block text-white font-bold text-xl'>React Jobs</span>
-            </section>
-            <section className='flex items-center justify-between p-2'>
-              <ul className='flex gap-2 md:gap-4 text-white'>
-                <li className='p-2 hover:bg-black rounded-md'><Link to='/'>Home</Link></li>
-                <li className='p-2 hover:bg-black rounded-md'><Link to='/jobs'>Jobs</Link></li>
-                <li className='p-2 hover:bg-black rounded-md'><Link to='/add-job'>Add Job</Link></li>
-              </ul>
-            </section>
-          </div>
-        </nav>
+      <nav className="bg-indigo-700 border-b-2 border-indigo-500">
+        <div className="flex items-center justify-center md:justify-between max-w-5xl w-full mx-auto px-2 sm:px-6 lg:px-8">
+          <section className="flex flex-shrink-0 items-center gap-1">
+            <img src={logo} alt="React Logo" className="h-10 w-auto" />
+            <span className="hidden sm:block text-white font-bold text-xl">
+              React Jobs
+            </span>
+          </section>
+          <section className="flex items-center justify-between p-2">
+            <ul className="flex gap-2 md:gap-4 text-white">
+              <li className="p-2 hover:bg-black rounded-md">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="p-2 hover:bg-black rounded-md">
+                <Link to="/jobs">Jobs</Link>
+              </li>
+              <li className="p-2 hover:bg-black rounded-md">
+                <Link to="/add-job">Add Job</Link>
+              </li>
+            </ul>
+          </section>
+        </div>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
