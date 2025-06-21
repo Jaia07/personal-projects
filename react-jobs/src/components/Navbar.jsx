781e47418d41881e21d0aaad148d813b5fcfc781
Joseph Adebanjo <josephadebanjo4@gmail.com>
2025-06-21 06:03:05 +0100

    Modified the navigation links to be highlighted when selected

--- react-jobs/src/components/Navbar.jsx
+++ react-jobs/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import logo from "../assets/images/logo.png";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const linkClass = ({ isActive }) => {
+    return isActive
+      ? "bg-black text-white p-2 hover:bg-black rounded-md"
+      : "p-2 hover:bg-black rounded-md text-white";
+  };
+
   return (
     <>
       <nav className="bg-indigo-700 border-b-2 border-indigo-500">
@@ -16,13 +22,19 @@ const Navbar = () => {
           <section className="flex items-center justify-between p-2">
             <ul className="flex gap-2 md:gap-4 text-white">
               <li className="p-2 hover:bg-black rounded-md">
-                <Link to="/">Home</Link>
+                <NavLink to="/" className={linkClass}>
+                  Home
+                </NavLink>
               </li>
               <li className="p-2 hover:bg-black rounded-md">
-                <Link to="/jobs">Jobs</Link>
+                <NavLink to="/jobs" className={linkClass}>
+                  Jobs
+                </NavLink>
               </li>
               <li className="p-2 hover:bg-black rounded-md">
-                <Link to="/add-job">Add Job</Link>
+                <NavLink to="/add-job" className={linkClass}>
+                  Add Job
+                </NavLink>
               </li>
             </ul>
           </section>
