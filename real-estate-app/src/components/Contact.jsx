67903bc36346321855247bb34152bd0eaf5e646a
Joseph Adebanjo <josephadebanjo4@gmail.com>
2025-09-01 08:15:23 +0100

    Implemented contact section with toast notification and email updates

---
+++ real-estate-app/src/components/Contact.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { toast } from "react-toastify";
+
+const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const submitForm = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "e6471f3c-7a17-4583-bad0-10bbd36a41e6");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("");
+      toast.success("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      toast.error(data.message);
+      setResult("");
+    }
+  };
+
+  return (
+    <section
+      className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden"
+      id="Contact"
+    >
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
+        Contact{" "}
+        <span className="underline underline-offset-4 decoration-1 font-light">
+          With us
+        </span>
+      </h1>
+      <p className="text-center text-gray-500 mb-12 max-w-80 mx-auto">
+        Ready to Make a Move? Let's Build Your Future Together
+      </p>
+
+      <form
+        onSubmit={submitForm}
+        className="max-w-2xl mx-auto text-gray-600 pt-8"
+      >
+        <section className="flex flex-wrap">
+          <div className="w-full md:w-1/2 text-left md:pl-4">
+            Your Name
+            <input
+              type="text"
+              name="Name"
+              placeholder="Your Name"
+              className="w-full border border-gray-300 rounded py-3 px-4 mt-2"
+              required
+            />
+          </div>
+          <div className="w-full md:w-1/2 text-left md:pl-4">
+            Your Email
+            <input
+              type="email"
+              name="Email"
+              placeholder="Your Email"
+              className="w-full border border-gray-300 rounded py-3 px-4 mt-2"
+              required
+            />
+          </div>
+        </section>
+        <section className="my-6 text-left md:pl-4">
+          Message
+          <textarea
+            className="w-full border border-gray-300 rounded py-3 px-4 mt-2 h-48 resize-none"
+            name="Message"
+            placeholder="Message"
+            required
+          ></textarea>
+        </section>
+        <button className="bg-blue-600 text-white py-2 px-12 mb-10 rounded">
+          {result ? result : "Send Message"}
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default Contact;
