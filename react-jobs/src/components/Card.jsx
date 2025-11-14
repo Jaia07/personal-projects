cd0cf05a3154f0436e23e86fbb887b28b1cb332d
Joseph Adebanjo <josephadebanjo4@gmail.com>
2025-11-14 05:06:34 +0100

    Revert "Merge branch 'main' of https://github.com/Jaia07/personal-projects"
    This reverts commit 6c098609ad92406f286d0447b980986676b431d6, reversing
    changes made to 60b008a65bebbc72309f0d5df72e5fd72ca86d3c.

--- react-jobs/src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-const Card = ({ children, bg = "bg-gray-100" }) => {
-  return (
-    <section className={`${bg} p-4 rounded-lg shadow-md min-w-[40vw]`}>
-      {children}
-    </section>
-  );
-};
-
-export default Card;
